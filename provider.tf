f2f7003be5b99f86d261a9f8d7b3cbf79d360bf9
Brij99 <124493575+Brij99@users.noreply.github.com>
2025-12-19 11:17:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-
-
+provider "aws" {
+  region = "ap-south-1"
+}
